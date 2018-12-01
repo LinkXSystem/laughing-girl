49f2674302ccda573ef56aba008f14a471e73637
zhi <linkasde_system@163.com>
2018-12-01 22:44:29 +0800

    :tada: project initial

---
+++ index.js
@@ -0,0 +1,7 @@
+const girl = name => {
+  console.log('=================================================');
+  console.log(`${name}, a girl who is loves to laugh !`);
+  console.log('=================================================');
+};
+
+module.export = girl;
